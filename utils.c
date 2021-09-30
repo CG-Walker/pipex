d2479e1b41deba2d7e49b6c150f61001ded6e401
CG-Walker <cogo97@hotmail.com>
2021-09-30 13:43:06 +0200

    Ajout de utils

---
+++ utils.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cgoncalv <cgoncalv@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/22 15:17:16 by cgoncalv          #+#    #+#             */
+/*   Updated: 2021/09/22 15:18:37 by cgoncalv         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex.h"
+
+void	free_double_ptr(char **str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i])
+	{
+		free(str[i]);
+		str[i] = NULL;
+		i++;
+	}
+	free(str);
+	str = NULL;
+}
+
+void	usage(void)
+{
+	printf("Usage : file1 cmd1 cmd2 file2\n");
+}
+
+char	*get_path(char **env)
+{
+	size_t	i;
+	char	*path;
+
+	i = 0;
+	path = NULL;
+	while (env[i])
+	{
+		if (ft_strncmp(env[i], "PATH=", 5) == 0)
+		{
+			path = (env[i] + 5);
+			break ;
+		}
+		i++;
+	}
+	return (path);
+}
